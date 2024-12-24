9ce6e077d613e65c57bfc66976a8c5a5b5610f33
PEDRO TOMPSEN <65947483+t0mpseN@users.noreply.github.com>
2024-12-24 17:44:38 -0300

    Update install_dependencies.bat

--- install_dependencies.bat
+++ install_dependencies.bat
@@ -4,4 +4,4 @@ REM Install Python dependencies from manga-ocr requirements
 pip install -r manga-ocr\manga_ocr_dev\requirements.txt
 
 REM Install Python dependencies for your scripts
-pip install pynput
+pip install pynput tkinter customtkinter PIL pystray screeninfo
